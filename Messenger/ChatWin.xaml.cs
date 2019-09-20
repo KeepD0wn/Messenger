b2c2263771758e4f1acae3a8829ece7f84abbb78
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2019-09-20 12:27:04 +0300

    Initial commit

---
+++ Messenger/ChatWin.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatWin.xaml
+    /// </summary>
+    public partial class ChatWin : Window
+    {       
+        public List<string[]> data = new List<string[]>();
+        MainWindow main = new MainWindow();
+
+        public ChatWin()
+        {
+            InitializeComponent();
+            Thread th = new Thread(UpdateTable);
+            th.Start();
+        }
+
+        public void UpdateTable() 
+        {
+            while (true)
+            {
+                ClearTable();
+                string qu = "select * from MessengerMessege";
+                data.Clear();
+                using (SqlCommand com = new SqlCommand(qu, main.Connect))
+                {
+                    SqlDataReader reader = com.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        data.Add(new string[1]);
+                        data[data.Count - 1][0] = reader[2].ToString();
+                    }
+
+                    foreach (string[] s in data)
+                    {
+                        Dispatcher.BeginInvoke(new ThreadStart(delegate { lb.Items.Add(s[0]); }));                       
+                    }
+                    reader.Close();
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        public void ClearTable()
+        {
+            foreach (string[] s in data)
+            {                
+                Dispatcher.BeginInvoke(new ThreadStart(delegate { lb.Items.Clear(); }));
+            }
+        }
+
+        private void TxtPost_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = string.Format($"Insert into MessengerMessege (MessegeUserID,MessegeText,MessegeDate) values (1,'{UserTxt.Text}',GETDATE())");
+            using (SqlCommand cmd = new SqlCommand(sql, main.Connect))
+            {                
+                    cmd.ExecuteNonQuery();     
+            }
+
+            UserTxt.Text = string.Empty;
+        }               
+    }
+}
---
+++ Messenger/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        private SqlConnection connect = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connect = new SqlConnection("Server=31.31.196.89; Database=u0805163_2iq; User Id=u0805163_user1; Password=1337Elit72;");
+            Connect.Open();           
+        }
+
+        public SqlConnection Connect { get { return connect; }}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {         
+            if(Logintb.Text=="test" && Passwtb.Text == "12345")
+            {
+                ChatWin cw = new ChatWin();
+                cw.Show();
+                this.Close();
+            }
+            else
+            {
+                Logintb.Text = string.Empty;
+                Passwtb.Text = string.Empty;
+            }
+        }
+
+    }
+}
